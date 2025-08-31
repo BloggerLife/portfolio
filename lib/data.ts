acdc191f27346837994e2bebad3685c7855e4969
BloggerLife <macarnoldnenyasha@gmail.com>
2025-08-31 13:02:26 +0200

    projects

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import cakes from "@/public/cakes.png";
+import guest from "@/public/guest.png";
+import blog from "@/public/blog.png";
 
 export const links = [
   {
@@ -62,25 +62,25 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "CakesByAmina",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
+      "An Online Cake Store (Fullstack), web application build with Javascript with the help of Nextjs ",
     tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+    imageUrl: cakes,
   },
   {
-    title: "rmtDev",
+    title: "Guest House Assist",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+      "Guest House Assist is a fullstack accommodation booking website, fully responsive, built with Javascript and Nextjs.",
     tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+    imageUrl: guest,
   },
   {
-    title: "Word Analytics",
+    title: "WesayBlog",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+      "The site can accommodate many different users and all the users can have different roles and posts, with the help of authentication features.",
+    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Framer"],
+    imageUrl: blog,
   },
 ] as const;
 
